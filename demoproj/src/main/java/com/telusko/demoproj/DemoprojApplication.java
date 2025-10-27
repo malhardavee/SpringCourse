9a36d942ba8a09b5b54e15deb3108adf2f6fc4b7
malhardavee <malhardavepc@gmail.com>
2025-10-27 10:18:23 +0530

    first commit

---
+++ demoproj/src/main/java/com/telusko/demoproj/DemoprojApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.demoproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoprojApplication.class, args);
+	}
+
+}
